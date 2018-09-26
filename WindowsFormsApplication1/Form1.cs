1e3f536277ca4c86504f13627da30038cc6169b2
Andrey <kalugin@outlook.com>
2018-09-26 17:06:11 +0300

    upd

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -17,6 +17,7 @@ namespace WindowsFormsApplication1
         delegate void SetTextCallback1(string text);
         private void SetText(string text)
         {
+            text = Accessory.FilterZeroChar(text);
             // InvokeRequired required compares the thread ID of the
             // calling thread to the thread ID of the creating thread.
             // If these threads are different, it returns true.
